079e32c9da358c36e2a0edec120a9c9ebc930644
J.T. Robinson <jaittarius@hotmail.com>
2019-04-21 14:22:16 -0400

    Added code auto-completion and beautify

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as ace from 'ace-builds'; // ace module ..
-// language package, choose your own 
+import 'ace-builds/src-noconflict/ext-beautify'; // code beautify
+// language package, choose your own
 import 'ace-builds/src-noconflict/mode-javascript';
 // ui-theme package
-import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-twilight';
 
-const THEME = 'ace/theme/github';
+const THEME = 'ace/theme/twilight';
 const LANG = 'ace/mode/javascript';
 
 @Component({
@@ -17,21 +18,45 @@ export class HomepageComponent implements OnInit {
 
   @ViewChild('codeEditor') codeEditorElmRef: ElementRef;
   private codeEditor: ace.Ace.Editor;
+  private editorBeautify: any;
 
   constructor() { }
 
   ngOnInit() {
+    ace.require('ace/ext/language_tools');
+    const element = this.codeEditorElmRef.nativeElement;
+    const editorOptions: Partial<ace.Ace.EditorOptions> = {
+      highlightActiveLine: true,
+      minLines: 10,
+      maxLines: Infinity,
+    };
+  // Set code editor options
+    this.codeEditor = ace.edit(element, editorOptions);
+    this.codeEditor.setTheme(THEME);
+    this.codeEditor.getSession().setMode(LANG);
+    this.codeEditor.setShowFoldWidgets(true); // for the scope fold feature
 
-  const element = this.codeEditorElmRef.nativeElement;
-  const editorOptions: Partial<ace.Ace.EditorOptions> = {
-    highlightActiveLine: true,
-    minLines: 10,
-    maxLines: Infinity,
-  };
+    this.editorBeautify = ace.require('ace/ext/beautify');
+  }
+  // Enable autocompletion
+  private getEditorOptions(): Partial<ace.Ace.EditorOptions> & { enableBasicAutocompletion?: boolean; } {
+    const basicEditorOptions: Partial<ace.Ace.EditorOptions> = {
+      highlightActiveLine: true,
+      minLines: 14,
+      maxLines: Infinity,
+    };
 
-  this.codeEditor = ace.edit(element, editorOptions);
-  this.codeEditor.setTheme(THEME);
-  this.codeEditor.getSession().setMode(LANG);
-  this.codeEditor.setShowFoldWidgets(true); // for the scope fold feature
-}
+    const extraEditorOptions = {
+      enableBasicAutocompletion: true
+    };
+    const margedOptions = Object.assign(basicEditorOptions, extraEditorOptions);
+    return margedOptions;
+  }
+  // Enable Code Beautify
+  public beautifyContent() {
+    if (this.codeEditor && this.editorBeautify) {
+      const session = this.codeEditor.getSession();
+      this.editorBeautify.beautify(session);
+    }
+  }
 }
